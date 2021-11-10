9e7e5fdffaa139ba529f8ce360f6ff6605f3e6ad
Mariii-23 <marianadinis8@hotmail.com>
2021-11-10 08:05:57 +0000

    Add materia

---
+++ 1sem/IA/fichas/ficha05.pl
@@ -0,0 +1,16 @@
+% Questao 1
+% Problema de estado único.
+
+%------------------------------------------------
+% Questao 2
+%
+% Conjunto de Estados: Conjunto de pares (X,Y), X representa o número de litros contidos no jarro de 8 litros de capacidade e o Y representa o número de litros contids no jarro de 5 litros de capacidade
+
+% Estado inicial: Jarros com 0 litros (0,0)
+
+% Estado objetivo: Um dos jarros com 4 l (_,4) ou (4,_)
+
+% Operadores: esvaziar um balde, encher um balde, esvaziar um balde para outro (nunca podendo ultrapassar a capacidade máxima do balde que se esta a encher ou ate que o outro balde fique vazio).
+
+%------------------------------------------------
+% Questao 3
