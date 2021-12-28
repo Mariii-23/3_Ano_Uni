94eb6de2f1106fd774cd2154c56c6c68776d5239
Mariii-23 <marianadinis8@hotmail.com>
2021-12-28 00:53:28 +0000

    add Dss testes

--- 1sem/IA/fichas/ficha9/ficha09.pl
+++ 1sem/IA/fichas/ficha9/ficha09.pl
@@ -1,13 +1,73 @@
 
 :- dynamic -/1.
 
--voo(X):- nao(voo(X)),
-          nao(excecao(-voo(X))).
+%% -voo(X):- nao(voo(X)),
+%%           nao(excecao(-voo(X))).
 
--voo(X):- mamifero(X),
-          nao(excecao(-voo(X))).
+%% -voo(X):- mamifero(X),
+%%           nao(excecao(-voo(X))).
 
-voo(X):- ave(X).
+%% voo(X):- ave(X).
 
-excecao(-voo(X)):- morcego(X).
-excecao(+voo(X)):- pinguim(X).
+%% excecao(-voo(X)):- morcego(X).
+%% excecao(+voo(X)):- pinguim(X).
+
+%1
+voa(X) :- ave(X), nao(excecao(voa(X))).
+
+%2
+-voa(X)  :- mamifero(X),nao( excecao(-voa(X))).
+
+%aux predicates
+-voa(X) :- excecao(voa(X)).
+voa(X) :- excecao(-voa(X)).
+
+%3
+-voa( tweety ).
+
+%aux predicates - definir as exceções
+excecao(voa(X)) :- avestruz(X).
+excecao(voa(X)) :- pinguim(X).
+excecao(-voa(X)) :- morcego(X).
+
+%4
+ave(pitigui).
+
+%5
+ave(X) :- canario(X).
+
+%6
+ave(X) :- piriquito(X).
+
+%7
+canario(piupiu).
+
+%8
+mamifero(silvestre).
+
+%9
+mamifero( X ) :- cao( X ).
+
+%10
+mamifero( X ) :- gato( X ).
+
+%11
+cao(boby).
+
+%12
+ave(X) :- avestruz(X).
+
+%13
+ave(X) :- pinguim(X).
+
+%14
+avestruz(trux).
+
+%15
+pinguim(pingu).
+
+%16
+mamifero(X) :- morcego(X).
+
+%17
+morcego(batemene).
