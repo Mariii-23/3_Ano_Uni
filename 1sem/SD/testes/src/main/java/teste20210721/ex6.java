772c8f0f7878d4b14677aeec7148d60ea1b88f71
Mariii-23 <marianadinis8@hotmail.com>
2022-01-18 00:33:38 +0000

    Add teste.org

--- 1sem/SD/testes/src/main/java/teste20210721/ex6.java
+++ 1sem/SD/testes/src/main/java/teste20210721/ex6.java
@@ -1,7 +1,5 @@
 package teste20210721;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,15 +10,14 @@ public class ex6 {
     }
 
     public static class gestaoVacinas implements ControloVacinas {
-        boolean acabar = false;
         private int vacinas = 0;
-        private Queue<Condition> pedidos;
+        private int ticket = 0;
+        private int aVacinar = 0;
 
         private final ReentrantLock l = new ReentrantLock();
         private final Condition c = l.newCondition();
 
         public gestaoVacinas() {
-          pedidos = new ArrayDeque<>();
         }
 
         @Override
@@ -29,11 +26,7 @@ public class ex6 {
                 l.lock();
                 System.out.println(Thread.currentThread().getName() + " -> Forneci vacinas: " + frascos);
                 this.vacinas += frascos;
-
-                while (!pedidos.isEmpty() && vacinas > 0) {
-                    pedidos.remove().signalAll();
-                    vacinas--;
-                }
+                c.signalAll();
 
             } finally {
                 l.unlock();
@@ -44,29 +37,83 @@ public class ex6 {
         public void pedirParaVacinar() throws InterruptedException {
             try {
                 l.lock();
-                System.out.println(Thread.currentThread().getName() + " -> Espera da vacina");
-
-                if (pedidos.isEmpty() && vacinas>0) {
-                    return;
+                int ticketPessoa = ticket++;
+                System.out.println(Thread.currentThread().getName() + " -> Espera da vacina -> "+ ticketPessoa);
+                c.signalAll();
+                while (vacinas <= 0 || ticketPessoa != aVacinar) {
+                    c.await();
                 }
-
-                Condition aux = l.newCondition();
-                pedidos.add(aux);
-                //c.signal();
-                aux.await();
-
                 System.out.println(Thread.currentThread().getName() + " -> Fui VACINADO.");
+                vacinas--;
+                aVacinar++;
+                c.signalAll();
+
             } finally {
                 l.unlock();
             }
         }
     }
 
+
+    public static class CentroVacinacaoPires implements ControloVacinas {
+        private int numVacinas;
+        private int numPessoasEspera;
+        private int NUM;
+        private int ticketAtual;
+        private final ReentrantLock l;
+        private final Condition pessoaEspera;
+
+        public CentroVacinacaoPires(int NUM) {
+            this.NUM = NUM;
+            this.numPessoasEspera = 0;
+            this.numVacinas = 0;
+            this.ticketAtual = 0;
+            this.l = new ReentrantLock();
+            this.pessoaEspera = l.newCondition();
+        }
+
+        @Override
+        public void pedirParaVacinar() throws InterruptedException {
+            try{
+                int ticketAqui = ticketAtual++;
+                l.lock();
+                numPessoasEspera++;
+                System.out.println(Thread.currentThread().getName() + " -> Espera da vacina -> "+ ticketAqui);
+                //Porque podem estar à espera porque ainda não há pessoas suficientes.
+                pessoaEspera.signalAll();
+                while(numVacinas > 0 && numPessoasEspera < NUM && ticketAqui > ticketAtual) {
+                    pessoaEspera.await();
+                }
+                numPessoasEspera--;
+                numVacinas = numVacinas - 1 / NUM;
+                System.out.println(Thread.currentThread().getName() + " -> Fui VACINADO.");
+            }
+            finally {
+                l.unlock();
+            }
+        }
+
+        @Override
+        public void fornecerFrascos(int frascos){
+            try{
+                l.lock();
+                System.out.println(Thread.currentThread().getName() + " -> Forneci vacinas: " + frascos);
+                numVacinas+=frascos;
+                pessoaEspera.signalAll();
+            }
+            finally {
+                l.unlock();
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         ControloVacinas gestaoVacinas = new gestaoVacinas();
-        int pedidos = 20;
+        //ControloVacinas gestaoVacinas = new CentroVacinacaoPires(10);
+        int pedidos = 10;
         int frascos = 2;
-        int entregas = 10;
+        int entregas = 5;
         int N = pedidos+entregas;
 
         Thread[] t = new Thread[N*2+1];
@@ -80,9 +127,7 @@ public class ex6 {
                         }
                     });
                 } else {
-                    t[i] = new Thread(() -> {
-                        gestaoVacinas.fornecerFrascos(frascos);
-                    });
+                    t[i] = new Thread(() -> gestaoVacinas.fornecerFrascos(frascos));
                 }
         }
         for (int i = 0 ; i < N ; i++) {
@@ -95,22 +140,17 @@ public class ex6 {
                     }
                 });
             } else if (i == N-1) {
-                t[i+N] = new Thread(() -> {
-                    gestaoVacinas.fornecerFrascos(frascos);
-                });
+                t[i+N] = new Thread(() -> gestaoVacinas.fornecerFrascos(frascos));
             } else {
-                t[i+N] = new Thread(() -> {
-                    gestaoVacinas.fornecerFrascos(frascos);
-                });
+                t[i+N] = new Thread(() -> gestaoVacinas.fornecerFrascos(frascos));
             }
         }
 
-        t[N*2] = new Thread(() -> {
-            gestaoVacinas.fornecerFrascos(pedidos*2);
-        });
+        t[N*2] = new Thread(() -> gestaoVacinas.fornecerFrascos(pedidos*2));
 
         for (int i = 0; i < 2*N; i++)
             t[i].start();
+        t[N*2].start();
         for (int i = 0; i < 2*N; i++) {
             try {
                 t[i].join();
