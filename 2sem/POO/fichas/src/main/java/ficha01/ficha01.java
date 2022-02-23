fb8642a47da7b23d56cdda82ff3ef73019e1c994
Mariii-23 <marianadinis8@hotmail.com>
2022-02-23 14:15:58 +0000

    Add PL and RC notes

---
+++ 2sem/POO/fichas/src/main/java/ficha01/ficha01.java
@@ -0,0 +1,13 @@
+package ficha01;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ficha01 {
+    public void ex1(int dia, int mes) {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Exercicio 1");
+    }
+}
